a6b58376403d961f59f5c3e4a7efafd3d0b96eb1
Thomas Schmitt <schmittt@amazon.com>
2015-05-08 22:01:36 +0000

    Configured job worker to automatically start on boot
    Set chkconfig to on for the init script if the job worker is deployed using
    AWS CodeDeploy.

--- codedeploy/scripts/application-start.sh
+++ codedeploy/scripts/application-start.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
+chkconfig aws-codepipeline-jobworker on
 /etc/init.d/aws-codepipeline-jobworker start "com.amazonaws.codepipeline.jobworker.configuration.CustomActionJobWorkerConfiguration"
